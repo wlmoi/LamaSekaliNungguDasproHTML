206464f9e4c3ee9271acd7460978ffc375248b56
William Anthony <147981830+wlmoi@users.noreply.github.com>
2024-06-28 19:56:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("predictBtn").addEventListener("click", function() {
+    var predictions = [
+        "Belum keluar malam ini Fergusoo!!! Jangan diperiksa",
+        "Siap-siap begadang malam, muncul malam ini deck!",
+        "Indeksnya ngga bakal muncul, haha",
+        "Mending cari ayang saja daripada nungguin",
+        "Lho, masih refresh SIX kamu?",
+        "Kenapa malah pencet, takut tidak lulus ya daspronya huhu",
+        "SIX asix SIX asix nunggu dirinya, ngga muncul dek",
+        "OMG MUNCUL DASPRO A!!!... BERCYANDA",
+        "Yakali mau kasih, kan kalau kalian ngerjain tubes deadliner, kami jugalah",
+        "Kayaknya kamu A deh mas/mbak, cek dulu SIX nya!!!"
+    ];
+    
+    var randomIndex = Math.floor(Math.random() * predictions.length);
+    var predictionElement = document.getElementById("prediction");
+    
+    predictionElement.classList.remove("show");
+    predictionElement.classList.add("hide");
+    
+    setTimeout(function() {
+        predictionElement.innerText = predictions[randomIndex];
+        predictionElement.classList.remove("hide");
+        predictionElement.classList.add("show");
+    }, 300);
+});
